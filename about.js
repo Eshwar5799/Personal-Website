7a6269a3991f73cbc4b3832b395b99e897874db7
Eshwar5799 <eshwarsv998@gmail.com>
2020-02-07 16:28:33 +0530

    Need to add JAVAscript

---
+++ about.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".element",{
+    strings:['Software Engg Student','Tech Enthusiast','Developer','Computer geek'],
+   smartBackspace:true,
+   typeSpeed: 60,
+   backSpeed:60,
+   loop:true
+});
