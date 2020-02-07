988631c56675e0c4e7369f16fe456d92eb2da470
Eshwar5799 <eshwarsv998@gmail.com>
2020-02-07 18:30:56 +0530

    Scroll Functionality Enabled

--- about.js
+++ about.js
@@ -5,3 +5,29 @@ var typed=new Typed(".element",{
    backSpeed:60,
    loop:true
 });
+// JAVAscript to enable Scrolling down
+
+//About Section
+/**let aboutHeight=document.getElementById("aboutImagediv").clientHeight;
+console.log("The About Div Height is: " + aboutHeight);
+//Experience Section
+let ExperienceHeight=document.getElementById("Experiencediv").clientHeight;
+console.log("The Experience Div Height is: " +ExperienceHeight );
+
+//Education Section
+
+//Skills Section
+let EducationHeight=document.getElementById("Skillsdiv").clientHeight;
+console.log("The skills Div Height is: " + EducationHeight);
+
+//Projects Section
+
+// Interests Section
+let InterestsHeight=document.getElementById("Interestssdiv").clientHeight;
+console.log("The Interests Div height is: " + InterestsHeight);
+
+//Contact Section
+let Contactsheight=document.getElementById("Contactsdiv").clientHeight;
+console.log("The Contacts Div height is: " + Contactsheight);
+**/
+
